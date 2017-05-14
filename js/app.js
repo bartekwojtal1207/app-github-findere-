f9271db4d5364561c37437ecfb7fe6e3904b43f5
bartekwojtal1207 <bartekwojtal1207@gmail.com>
2017-05-14 21:07:56 +0200

    app finder
    add valid in #ajax

--- js/app.js
+++ js/app.js
@@ -1,36 +1,35 @@
 $(document).ready(function(){
   var input_search = $(".input_search");
+  var panel_result = $(".search_result");
+
 
   input_search.on("keyup",function(e){
     let value_input = e.target.value;
-    //console.log(value_input);
     var counter = input_search.val().length;
     check_length(counter);
 
     $.ajax({
-    //  url: "https://api.github.com/users/"+value_input
       url: "https://api.github.com/search/repositories?q=" + value_input+"&page=1&per_page=10",
       type : "get"
-      // https://api.github.com/search/repositories?q=topic:ruby+topic:rails
-
-    }).done(function(repositories){
-        console.log(repositories.items[0]);
+      }).done(function(repositories){
+        let x = repositories.items[0];
+          if(typeof x == "undefined") {
+            console.log('nie ma ');
+            panel_result.html("szukaj");
+          }else{
+            console.log(x);
+            panel_result.html(x.name)
+            console.log('jest');
+          }
         console.log("done");
-    });
-
-    console.log("fail");
+      }).fail(function(error){
+        console.log("fail");
+      });
   });
-
-
-
 
   function check_length(counter){
     if (counter <=3 ) {
-    //  console.log('za malo literek');
-
     };
   };
-
-
 
 });

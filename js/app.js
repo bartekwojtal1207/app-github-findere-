4d362a7a4cf81fa28273913a6da357445ef5d5bb
bartekwojtal1207 <bartekwojtal1207@gmail.com>
2017-05-15 20:31:30 +0200

    git repo finder
    95% success

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   var input_search = $(".input_search");
+  var btn = $("#search");
   var panel_result = $(".search_result");
 
 
-  input_search.on("keyup",function(e){
-    let value_input = e.target.value;
+  btn.on("click",function(e){
+    e.preventDefault();
+    let value_input = input_search.val();
     var counter = input_search.val().length;
 
     $.ajax({
@@ -14,21 +16,15 @@ $(document).ready(function(){
         client_id : "6be82356726ebd3a4b7f",
         client_secret : "8b52d99f3e5af6a5b5f11bae8179e612b8c6ec14"
       }
-      }).done(function(repositories){
+    }).done(function(repositories){//conect
         let repo_item = repositories.items;
-
-        if (counter <4 ) {
+        if (counter <4 ) {// if symbol < 4
           $(".list").children().remove();
-          $("h3").css("display","block");
-          $("h4").css("display","none");
-          $("h5").css("display","none");
-        }else{
-          $("h3").css("display","none");
-          $("h4").css("display","none");
-          $("h5").css("display","block");
-          serch_repo(repo_item);
+          $(".waring_title").fadeIn(1000);// show message
+        }else{// if not
+          $(".warnig_title").fadeOut(200);
+          serch_repo(repo_item); // run function search
         }
-          console.log(repo_item.length);
           console.log("done");
       }).fail(function(error){
         console.log("fail");
@@ -41,35 +37,37 @@ $(document).ready(function(){
 
 
   function serch_repo(repo_item,counter){
-    if(typeof repo_item == "undefined") {
-      console.log('nie ma ');
-      $("h3").css("display","none");
-      $("h4").css("display","block");
-      $("h5").css("display","none");
-      }else if (repo_item.length <1 ) {
-        $(".list").html("<h5>result : 0 !</h5>");
-        console.log('nie ma  : brak wyniku');
-        $("h3").css("display","none");
-        $("h4").css("display","none");
-        $("h5").css("display","block");
+    if(typeof repo_item == "undefined") { // if repo undefined
+      $(".waring_title").css("display","none");
+      $(".no_result_title").fadeIn(1000);
+    }else if (repo_item.length <1 ) { // if repo not found
+          $(".list").children().remove();
+          $(".waring_title").fadeOut(200);
+          $(".no_result_title").fadeIn(1000); //show message
       }else{
-        $(".list").html("<h4>Resluts:</h4>");
-        console.log('jest ');
-        $("h3").css("display","none");
-        $("h4").css("display","block");
-        $("h5").css("display","none");
+        $(".list").html(""); //clear list
+        $(".waring_title").fadeOut(200);
+        $(".no_result_title").fadeOut(200);  // hide message
           for (var i = 0; i < repo_item.length; i++) {
-            console.log( repo_item[i].owner.avatar_url);
-
-            var rep_name = $("<h6>"+repo_item[i].name+"</h6>");
-            var rep_full_name = $("<p>"+repo_item[i].full_name+"</p>");
-           var new_img = $("<img class='avatar' src="+repo_item[i].owner.avatar_url+"/>");
-           console.log(new_img);
+            var rep_name = $("<h3 class='name_repo_title'>"+repo_item[i].name+"</h6>");
+            var rep_full_name = $("<p class='full_name_repo_title'>"+repo_item[i].full_name+"</p>");
+            var new_img = $("<img class='avatar' src="+repo_item[i].owner.avatar_url+"/>");
             var new_li = $("<li>");
-      //      console.log(new_li);
-            $(".list").append(new_li);
-            $(new_li).append(new_img);
-            $(new_li).append(rep_full_name);
+            var new_link = $("<a href="+repo_item[i].owner.html_url+"/>");
+            var container_result_li = $("<div class='big_link'>");
+          console.log(repo_item[i]);
+           $(".list").append(new_li);
+            $(new_li).append(new_link);
+            $(new_link).append(container_result_li);
+              $(container_result_li).append(new_img);
+              $(container_result_li).append(rep_name);
+              $(container_result_li).append(rep_full_name);
+          //  $(new_link).append(rep_name);
+            //$(new_link).append(rep_full_name);
+            // $(new_li).append(new_img);
+            // $(new_li).append(rep_name);
+            // $(new_li).append(rep_full_name);
+
           }/// end pf loop
       }// end of else
   };// end of function
